3d9a5fb5e0a6cc760748c06d3d1d3fa0bed198a4
koji66 <takagame0127@gmail.com>
2026-04-27 18:48:19 +0900

    third

---
+++ app/api/generate/test/route.ts
@@ -0,0 +1,14 @@
+import { supabaseAdmin } from "@/lib/supabase";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const { data, error } = await supabaseAdmin
+    .from("users")
+    .select("*");
+
+  if (error) {
+    return NextResponse.json({ error });
+  }
+
+  return NextResponse.json({ data });
+}
